0c60dd3539d5763fc490a0258304a87ae6a87fbb
snowingfox <1503401882@qq.com>
2019-04-16 17:48:37 +0800

    Update ajax.js

--- js/ajax.js
+++ js/ajax.js
@@ -10,7 +10,14 @@ $(document).ready(function(){
 			{
 				
 			url:"wrongfile.txt",  //自主填写后台处理程序的文件地址
-
+			
+			type:"post",
+				
+			dataType:"text",
+				
+			//{'username':username,'password':password}	
+			data:"{'username':"+username+"'password':"+password+"}",
+				
 			error:function(xhr,statuet,data){
 				$(alertInfo[0]).show(300);
 				$(alertInfo[0]).text("发生了一个错误:"+statuet);
@@ -18,8 +25,16 @@ $(document).ready(function(){
 			},
 
 			success:function(result){
-				$(alertInfo[1]).show(300);
-				$(alertInfo).delay(2000).hide(300);
+				
+				if(result == "success"){
+					$(alertInfo[0]).show(300);
+					$(alertInfo[0]).text("登陆成功!");
+					$(alertInfo).delay(2000).hide(300);
+				}else{
+					$(alertInfo[0]).show(300);
+					$(alertInfo[0]).text("请检查用户名和密码是否输入正确!");
+					$(alertInfo).delay(5000).hide(300);
+				}
 			}
 
 
